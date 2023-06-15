e6e61c1867b06428a0ba7c7cd1b4c186c306ecd7
pcf26536 <muffwaindan@gmail.com>
2023-06-15 12:27:36 +0300

    add rotation commands

--- auto-monitor-scaling.sh
+++ auto-monitor-scaling.sh
@@ -1,2 +1,7 @@
-xrandr --output eDP-1 --scale 0.4999x0.4999
-xrandr --output "HDMI-1" --scale 0.9999x0.9999
+## External monitor not rotated
+# -xrandr --output eDP-1 --scale 0.4999x0.4999
+# -xrandr --output "HDMI-1" --scale 0.9999x0.9999
+
+## External monitor rotated right
+xrandr --output eDP-1 --scale 0.8749x0.8749 --right-of "HDMI-1"
+xrandr --output "HDMI-1" --scale 0.9999x0.9999 --rotate right
