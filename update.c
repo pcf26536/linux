bda65df37fe2dfd76fed1a63556b6d3e7c19731c
pcf26536 <muffwaindan@gmail.com>
2023-06-18 11:19:55 +0300

    system update program

---
+++ update.c
@@ -0,0 +1,12 @@
+/*
+   A program that executes a bash script
+   `/mnt/files/Repositories/linux/system-upgrade.sh`
+
+   The script is responsible for updating a manjaro system via `yay`
+*/
+#include <stdlib.h>
+
+int main()
+{
+    system("/mnt/files/Repositories/linux/system-upgrade.sh");
+}
